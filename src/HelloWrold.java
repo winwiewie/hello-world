e515c2ed6eef09f90b63b0ddd7c3f1f6367250e9
winwiewie <46095580+winwiewie@users.noreply.github.com>
2021-02-20 17:51:38 +0900

    initial commit

---
+++ src/HelloWrold.java
@@ -0,0 +1,8 @@
+
+public class HelloWrold {
+	public static void main(String[] args) {
+		System.out.println("Hello GOD World");
+		System.out.println("Hello jesus World");
+		System.out.println("Hello Holy Ghost(Spirit) Wold");
+	}
+}
